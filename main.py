4acb188d2bb30a1d7fefabe84471b333440d3bf1
Subash Mylraj <subashmylraj@gmail.com>
2020-11-15 17:47:59 +0300

    Fixed black print

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+import random
 maze = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 	[1,9,9,9,9,9,9,9,9,9,9,9,9,1,1,9,9,9,9,9,9,9,9,9,9,9,9,1],
 	[1,9,1,1,1,1,9,1,1,1,1,1,9,1,1,9,1,1,1,1,1,9,1,1,1,1,9,1],
@@ -11,10 +13,10 @@ maze = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 	[1,1,1,1,1,1,9,1,1,1,1,1,9,1,1,9,1,1,1,1,1,9,1,1,1,1,1,1],
 	[1,1,1,1,1,1,9,1,1,1,1,1,9,1,1,9,1,1,1,1,1,9,1,1,1,1,1,1],
 	[1,1,1,1,1,1,9,1,1,9,9,9,9,9,9,9,9,9,9,1,1,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,9,1,1,1,8,8,1,1,1,9,1,1,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,9,1,5,0,0,0,0,4,1,9,1,1,9,1,1,1,1,1,1],
+	[1,1,1,1,1,1,9,1,1,9,1,1,1,0,0,1,1,1,9,1,1,9,1,1,1,1,1,1],
+	[1,1,1,1,1,1,9,1,1,9,1,0,0,0,0,0,0,1,9,1,1,9,1,1,1,1,1,1],
 	[1,1,1,1,1,1,9,9,9,9,1,0,0,0,0,0,0,1,9,9,9,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,9,1,2,0,0,0,0,3,1,9,1,1,9,1,1,1,1,1,1],
+	[1,1,1,1,1,1,9,1,1,9,1,0,0,0,0,0,0,1,9,1,1,9,1,1,1,1,1,1],
 	[1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1],
 	[1,1,1,1,1,1,9,1,1,9,9,9,9,9,9,9,9,9,9,1,1,9,1,1,1,1,1,1],
 	[1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1],
@@ -60,7 +62,66 @@ class maze_map:
 			coord[1] += self.pixel_size
 			coord[0] = 0
 
-class ghost:
+
+class ghost ():
+	pixel_size = 20
+	pixel_center = pixel_size/2
+	coin_colour = (255, 182, 193)
+	coin_radius = 4
+	def __init__(self,coord,surface):
+
+		
+		self.coord=coord
+		self.surface=surface
+		self.ghost_colour=(0,255,0)
+		self.ghost_radius=10
+	
+	
+		
+	def draw(self):
+		pygame.draw.circle(self.surface, self.ghost_colour, (self.coord[0]*self.pixel_size+(self.pixel_center), self.coord[1]*self.pixel_size+(self.pixel_center)), self.ghost_radius)
+	
+	def cleardraw(self):
+		pygame.draw.rect(self.surface,(0,0,0), pygame.Rect(([self.pixel_size*i for i in self.coord]), (self.pixel_size, self.pixel_size))) 
+	
+	def draw_coin(self):
+		pygame.draw.circle(self.surface, self.coin_colour, (self.coord[0]*self.pixel_size+(self.pixel_center), self.coord[1]*self.pixel_size+(self.pixel_center)), self.coin_radius)
+		print("coin")
+	def move_ghost(self):
+    		
+		self.cleardraw()
+
+		
+		key_press = random.randint(0,3)
+		print(key_press)
+		tmp_x = self.coord[0]
+		tmp_y = self.coord[1]
+		if(maze[tmp_y][tmp_x] == 9):
+			self.draw_coin()
+			print("call-coin")
+		if(key_press == 0):
+			if(maze[tmp_y][tmp_x+1] != 1):
+				print("ghost-right")
+				self.coord[0]+=1
+			self.draw()
+		elif(key_press == 1):
+			if(maze[tmp_y][tmp_x-1]!=1):
+				print("ghost-left")
+				self.coord[0]-=1
+			self.draw()
+		elif(key_press == 2):
+			if(maze[tmp_y-1][tmp_x]!=1):
+				print("ghost-up")
+				self.coord[1]-=1
+			self.draw()
+		elif(key_press == 3):
+			if(maze[tmp_y+1][tmp_x]!=1):
+				print("ghost-down")
+				self.coord[1]+=1
+			self.draw()
+		
+		
+			
 
 
 class player(maze_map):
@@ -90,14 +151,17 @@ player_1.draw()
 mapobj= maze_map(surface)
 mapobj.show()
 
+ghost=ghost([11,13],surface)
+
 pygame.display.flip()
 
 clock = pygame.time.Clock()
 
 while True:
-
+	ghost.move_ghost()
 	keys = pygame.key.get_pressed()
 
+	# if event.type == pygame.KEYDOWN:
 	tmp_x=player_1.coord[0]
 	tmp_y=player_1.coord[1]
 	if keys[pygame.K_RIGHT]:
@@ -146,6 +210,5 @@ while True:
 			pygame.quit()
 			exit()
 
-	clock.tick(8)
+	clock.tick(2)
 	pygame.display.flip()
-	print("Score: %d" %coin_score)
