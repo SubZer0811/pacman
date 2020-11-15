d16a148a57e0290d806c8131aae0de7da4789218
Subash Mylraj <subashmylraj@gmail.com>
2020-11-15 23:42:52 +0300

    Cleaned Code

--- main.py
+++ main.py
@@ -1,82 +1,17 @@
 import pygame
 import sys
 import random
-maze = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-	[1,9,9,9,9,9,9,9,9,9,9,9,9,1,1,9,9,9,9,9,9,9,9,9,9,9,9,1],
-	[1,9,1,1,1,1,9,1,1,1,1,1,9,1,1,9,1,1,1,1,1,9,1,1,1,1,9,1],
-	[1,9,1,1,1,1,9,1,1,1,1,1,9,1,1,9,1,1,1,1,1,9,1,1,1,1,9,1],
-	[1,9,1,1,1,1,9,1,1,1,1,1,9,1,1,9,1,1,1,1,1,9,1,1,1,1,9,1],
-	[1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,1],
-	[1,9,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,9,1,1,9,1,1,1,1,9,1],
-	[1,9,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,9,1,1,9,1,1,1,1,9,1],
-	[1,9,9,9,9,9,9,1,1,9,9,9,9,1,1,9,9,9,9,1,1,9,9,9,9,9,9,1],
-	[1,1,1,1,1,1,9,1,1,1,1,1,9,1,1,9,1,1,1,1,1,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,1,1,1,9,1,1,9,1,1,1,1,1,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,9,9,9,9,9,9,9,9,9,9,1,1,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,9,1,1,1,0,0,1,1,1,9,1,1,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,9,1,0,0,0,0,0,0,1,9,1,1,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,9,9,9,1,0,0,0,0,0,0,1,9,9,9,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,9,1,0,0,0,0,0,0,1,9,1,1,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,9,9,9,9,9,9,9,9,9,9,1,1,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1],
-	[1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1],
-	[1,9,9,9,9,9,9,9,9,9,9,9,9,1,1,9,9,9,9,9,9,9,9,9,9,9,9,1],
-	[1,9,1,1,1,1,9,1,1,1,1,1,9,1,1,9,1,1,1,1,1,9,1,1,1,1,9,1],
-	[1,9,1,1,1,1,9,1,1,1,1,1,9,1,1,9,1,1,1,1,1,9,1,1,1,1,9,1],
-	[1,9,9,9,1,1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,1,1,9,9,9,1],
-	[1,1,1,9,1,1,9,1,1,9,1,1,1,1,1,1,1,1,9,1,1,9,1,1,9,1,1,1],
-	[1,1,1,9,1,1,9,1,1,9,1,1,1,1,1,1,1,1,9,1,1,9,1,1,9,1,1,1],
-	[1,9,9,9,9,9,9,1,1,9,9,9,9,1,1,9,9,9,9,1,1,9,9,9,9,9,9,1],
-	[1,9,1,1,1,1,1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,1,1,9,1],
-	[1,9,1,1,1,1,1,1,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,1,1,9,1],
-	[1,9,9,9,9,9,9,9,9,9,9,9,9,0,0,9,9,9,9,9,9,9,9,9,9,9,9,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+from maze import *
 
 coin_score=0
 
-class maze_map:
+class ghost (maze_map):
 
-	pixel_size = 20
-	pixel_center = pixel_size/2
-	wall_colour = (255,0,0)
-	coin_colour = (255, 182, 193)
-	coin_radius = 4
-	player_colour = (255, 255, 0)
-	player_radius = 8
-
-	def __init__(self, surface):
-		
-		self.surface = surface
-
-
-	def show(self):
-		coord = [0, 0]
-		for j in maze:
-			for i in j:
-				if (i == 1):
-					pygame.draw.rect(self.surface, self.wall_colour, pygame.Rect((coord), (self.pixel_size, self.pixel_size))) 
-				if (i == 9):
-					pygame.draw.circle(self.surface, self.coin_colour, (coord[0]+(self.pixel_center), coord[1]+(self.pixel_center)), self.coin_radius)
-				coord[0] += self.pixel_size
-			coord[1] += self.pixel_size
-			coord[0] = 0
-
-
-class ghost ():
-	pixel_size = 20
-	pixel_center = pixel_size/2
-	coin_colour = (255, 182, 193)
-	coin_radius = 4
 	def __init__(self,coord,surface):
-
-		
 		self.coord=coord
 		self.surface=surface
 		self.ghost_colour=(0,255,0)
-		self.ghost_radius=10
-	
-	
+		self.ghost_radius=10	
 		
 	def draw(self):
 		pygame.draw.circle(self.surface, self.ghost_colour, (self.coord[0]*self.pixel_size+(self.pixel_center), self.coord[1]*self.pixel_size+(self.pixel_center)), self.ghost_radius)
@@ -86,7 +21,7 @@ class ghost ():
 	
 	def draw_coin(self):
 		pygame.draw.circle(self.surface, self.coin_colour, (self.coord[0]*self.pixel_size+(self.pixel_center), self.coord[1]*self.pixel_size+(self.pixel_center)), self.coin_radius)
-		print("coin")
+		
 	def move_ghost(self):
     		
 		self.cleardraw()
@@ -107,13 +42,12 @@ class ghost ():
 		for i in range(len(way_mat)):
 			if(way_mat[i]):
 				test.append(i)
-		print("test: ", test)
-		key_press = random.randint(0,len(test)-1)
-		print(key_press)
-		key_press = test[key_press]
+
+		key_press = test[random.randint(0,len(test)-1)]
+		
 		if(maze[tmp_y][tmp_x] == 9):
 			self.draw_coin()
-			print("call-coin")
+			
 		if(key_press == 0):
 			if(maze[tmp_y][tmp_x+1] != 1):
 				print("ghost-right")
@@ -135,10 +69,6 @@ class ghost ():
 				self.coord[1]+=1
 			self.draw()
 		
-		
-			
-
-
 class player(maze_map):
 	
 	SIZE = (10, 10)
