14e6dba332af072c7c0a4691f5919d5b50820009
Subash Mylraj <subashmylraj@gmail.com>
2020-11-15 13:38:37 +0300

    player.py

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class player:
 
 	SIZE = (10, 10)
 	color = (255,0,0)
-	VELOCITY = 1
+	VELOCITY = 10
 
 	def __init__(self, x, y, surface):
 		
@@ -12,34 +12,31 @@ class player:
 		self.y = y
 		self.surface = surface
 
+	def cleardraw(self):
+		pygame.draw.rect(surface,(0,0,0),pygame.Rect((self.x, self.y), self.SIZE))
 
 	def draw(self):
-		pygame.draw.rect(surface, self.color, pygame.Rect((self.x, self.y), self.SIZE)) 
-
-	def play(self):
-
-		while True:
-			
+		pygame.draw.rect(surface, self.color, pygame.Rect((self.x, self.y), self.SIZE))
 
 
-if __name__ == "__main__":
-	
-	pygame.init()
+pygame.init()
 
-	surface = pygame.display.set_mode((400,300))
-	
-	player_1 = player(100, 100, surface)
-	player_1.draw()
-	pygame.display.flip()
+surface = pygame.display.set_mode((400,300))
 
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				exit()
-
-
-
-
-# # Drawing Rectangle 
-# pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60)) 
+player_1 = player(100, 100, surface)
+player_1.draw()
+pygame.display.flip()
+player_2 = player(500, 500, surface)
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			print("bfvndjfn")
+			pygame.quit()
+			exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				print("bfvndjfn")
+				player_1.cleardraw()
+				player_1.x+=player_1.VELOCITY
+				player_1.draw()
+		pygame.display.flip()
